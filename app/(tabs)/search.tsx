e1ac5a8376ffcc80dc919a3524fd138cb96049cc
Hassine Helmi <helmi.hassine1@gmail.com>
2025-10-31 12:19:52 +0100

    feat/homepage api calls success

---
+++ app/(tabs)/search.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+const search = () => {
+  return (
+
+    <SafeAreaView>
+      <Text>search</Text>
+    </SafeAreaView>
+  )
+}
+
+export default search
